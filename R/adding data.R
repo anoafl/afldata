3dfe1c112eab0e5f3dc9f4e7eea02f34a330ab95
Analysis of AFL <analysisofafl@gmail.com>
2018-02-26 13:43:47 +1100

    commit
    commit

--- R/adding data.R
+++ R/adding data.R
@@ -1,18 +1,18 @@
-library(readxl)
-# data2upload <- ("A:/OneDrive/PhD statistics/mixed models/margin.csv")
-data2upload <- read.csv("C:/Users/rober/Google Drive/afl_odds.csv")
-names(data2upload)
-library(devtools)
-afl_odds=data2upload
-use_data(afl_odds,overwrite = TRUE)
-
-devtools::check()
-
-library(devtools)
-devtools::install_github("anoafl/afldata")
-
-use_data(player_stats_basic,overwrite = TRUE)
-scoreprogression_unparsed=fitzRoy::scoreprogression_unparsed
-player_stats_basic=fitzRoy::player_stats_basic
-
-
+# library(readxl)
+# # data2upload <- ("A:/OneDrive/PhD statistics/mixed models/margin.csv")
+# data2upload <- read.csv("C:/Users/rober/Google Drive/afl_odds.csv")
+# # names(data2upload)
+# library(devtools)
+# afl_odds=data2upload
+# use_data(afl_odds,overwrite = TRUE)
+#
+# devtools::check()
+#
+# library(devtools)
+# devtools::install_github("anoafl/afldata")
+#
+# # use_data(player_stats_basic,overwrite = TRUE)
+# # scoreprogression_unparsed=fitzRoy::scoreprogression_unparsed
+# # player_stats_basic=fitzRoy::player_stats_basic
+#
+#
