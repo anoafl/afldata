7175f3059edaacebd954195daa58ad40aea20d48
Analysis of AFL <analysisofafl@gmail.com>
2018-05-15 14:46:57 +1000

    push
    push

--- R/adding data.R
+++ R/adding data.R
@@ -1,17 +1,21 @@
 # library(readxl)
-# # data2upload <- ("A:/OneDrive/PhD statistics/mixed models/margin.csv")
-# data2upload <- read.csv("C:/Users/rober/Google Drive/afl_odds.csv")
+# data2upload <- ("A:/OneDrive/PhD statistics/mixed models/margin.csv")
+# NBA_Games_Master
+#
+# NBA_Team_BoxScores_Extended
+#
+# data2upload <- NBA_Team_BoxScores_Extended
 # # names(data2upload)
 # library(devtools)
-# afl_odds=data2upload
-# use_data(afl_odds,overwrite = TRUE)
+# NBA_Team_BoxScores_Extended=data2upload
+# use_data(NBA_Team_BoxScores_Extended,overwrite = TRUE)
 #
 # devtools::check()
-#
-# library(devtools)
-devtools::install_github("jimmyday12/fitzroy")
-#
-# # use_data(player_stats_basic,overwrite = TRUE)
+# #
+# # library(devtools)
+# devtools::install_github("jimmyday12/fitzroy")
+# #
+# # # use_data(player_stats_basic,overwrite = TRUE)
 # # scoreprogression_unparsed=fitzRoy::scoreprogression_unparsed
 # # player_stats_basic=fitzRoy::player_stats_basic
 #
